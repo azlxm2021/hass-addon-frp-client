e17bfd53916df6cc3165438d51c52d560056c8d9
azlxm2021 <105138995+azlxm2021@users.noreply.github.com>
2025-08-28 00:53:28 +0800

    fix str
    fix str

--- frp-client/run.sh
+++ frp-client/run.sh
@@ -18,9 +18,9 @@ sed -i "s/webServer.user = \"admin\"/webServer.user = \"$(bashio::config 'webSer
 sed -i "s/webServer.password = \"123456789\"/webServer.password = \"$(bashio::config 'webServerPassword')\"/" $CONFIG_PATH
 sed -i "s/customDomains = \[\"your_domain\"\]/customDomains = [\"$(bashio::config 'customDomain')\"]/" $CONFIG_PATH
 sed -i "s/name = \"your_proxy_name\"/name = \"$(bashio::config 'proxyName')\"/" $CONFIG_PATH
-sed -i "s/name = \"your_local_port\"/name = \"$(bashio::config 'localPort')\"/" $CONFIG_PATH
-sed -i "s/name = \"your_local_ip\"/name = \"$(bashio::config 'localIP')\"/" $CONFIG_PATH
-sed -i "s/name = \"your_remote_port\"/name = \"$(bashio::config 'remotePort')\"/" $CONFIG_PATH
+sed -i "s/localPort = 123/localPort = $(bashio::config 'localPort')/" $CONFIG_PATH
+sed -i "s/localIP = "123"/localIP = $(bashio::config 'localIP')/" $CONFIG_PATH
+sed -i "s/remotePort = 123/remotePort = $(bashio::config 'remotePort')/" $CONFIG_PATH
 
 bashio::log.info "Starting frp client"
 
